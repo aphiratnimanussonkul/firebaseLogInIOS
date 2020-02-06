03938e4b0db5a651787f25720a0b57b6ddec215f
Aphirat Nimanussonkul <tata@odds.team>
2020-02-06 16:05:25 +0700

    added action to sign up if user use to sign up in this app

--- FirebaseLogin/controller/SignUpViewController.swift
+++ FirebaseLogin/controller/SignUpViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
@@ -6,7 +7,43 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var textFieldEmail: UITextField!
     @IBOutlet weak var textFieldPassword: UITextField!
     
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func signUpButtonTapped(_ sender: Any) {
+        guard let username = textFieldUserName.text,
+        username != "",
+        let email = textFieldEmail.text,
+        email != "",
+        let password = textFieldPassword.text,
+            password != "" else {
+                AlertController.showAlert(inViewController: self, title: "Missing Info", message: "Please fill out all fields!")
+                return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            
+            guard error == nil else {
+                AlertController.showAlert(inViewController: self, title: "Error", message: error!.localizedDescription)
+                return
+            }
+            
+            guard let user = authResult?.user else {
+                return
+            }
+
+            let changeRequest = user.createProfileChangeRequest()
+            changeRequest.displayName = username
+            changeRequest.commitChanges { (error) in
+                guard error == nil else {
+                    AlertController.showAlert(inViewController: self, title: "Error", message: error!.localizedDescription)
+                    return
+                }
+                
+                self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+            }
+        }
     }
     
     override func viewDidLoad() {
