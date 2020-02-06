6bade46d384fffa83a81165affe4828808f309a0
Aphirat Nimanussonkul <tata@odds.team>
2020-02-06 11:31:33 +0700

    Initial Commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by ODDS on 6/2/2563 BE.
+//  Copyright © 2563 ODDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
