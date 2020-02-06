0d9d5b61cfd760094970b7b11c2a7b73630d746e
Aphirat Nimanussonkul <tata@odds.team>
2020-02-06 16:04:44 +0700

    added code to sign in with firebase lib

--- FirebaseLogin/controller/SignInViewController.swift
+++ FirebaseLogin/controller/SignInViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
 
@@ -7,6 +8,26 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var textFieldPassword: UITextField!
     
     @IBAction func signInButtonTapped(_ sender: Any) {
+        guard let email = textFieldEmail.text,
+            email != "",
+            let password = textFieldPassword.text,
+            password != "" else {
+                AlertController.showAlert(inViewController: self, title: "Missing Info", message: "Please fill out all required fields")
+                return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (signResult, error) in
+            guard error == nil else {
+                AlertController.showAlert(inViewController: self, title: "Error", message: error!.localizedDescription)
+                return
+            }
+            
+            guard let user = signResult?.user else {
+                return
+            }
+            
+            self.performSegue(withIdentifier: "signInSegue", sender: nil)
+        }
     }
     
     override func viewDidLoad() {
