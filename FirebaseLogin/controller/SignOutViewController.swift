6dca306a2194142dac90726465dbe590907feeab
Aphirat Nimanussonkul <tata@odds.team>
2020-02-06 16:03:42 +0700

    added action when click sign out bin to pop viewController

--- FirebaseLogin/controller/SignOutViewController.swift
+++ FirebaseLogin/controller/SignOutViewController.swift
@@ -1,14 +1,28 @@
 import UIKit
+import Firebase
 
 class SignOutViewController: UIViewController {
 
-    @IBOutlet weak var labelName: UIButton!
+    @IBOutlet weak var labelName: UILabel!
     
     @IBAction func signOutButtonTapped(_ sender: UIButton) {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print(error)
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let username = Auth.auth().currentUser?.displayName else {
+            return
+        }
+        labelName.text = "Hello \(username)"
     }
     
 }
